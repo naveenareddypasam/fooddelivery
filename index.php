419ce372e57cb7f4a6b6a39c9c075e6fdf356d67
naveenareddypasam <149114756+naveenareddypasam@users.noreply.github.com>
2023-12-04 21:40:33 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+// Example PHP code (for illustration purposes)
+// You should add your own server-side logic as needed
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Process form submission
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    // Perform additional validation and processing as needed
+    // For a production environment, consider using a database to store form submissions
+}
+?>
