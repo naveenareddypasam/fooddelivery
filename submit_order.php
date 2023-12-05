7217cb00511bf30c1e19043c2d795415639e98cb
naveenareddypasam <149114756+naveenareddypasam@users.noreply.github.com>
2023-12-04 21:39:39 -0600

    Update submit_order.php

--- submit_order.php
+++ submit_order.php
@@ -13,37 +13,3 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 }
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles.css">
-    <title>Food Delivery</title>
-</head>
-<body>
-    <!-- Your existing HTML structure goes here -->
-
-    <section id="contact">
-        <h2>Contact Us</h2>
-        <form method="post" action="index.php">
-            <label for="name">Name:</label>
-            <input type="text" id="name" name="name" required>
-
-            <label for="email">Email:</label>
-            <input type="email" id="email" name="email" required>
-
-            <label for="message">Message:</label>
-            <textarea id="message" name="message" required></textarea>
-
-            <button type="submit">Submit</button>
-        </form>
-    </section>
-
-    <footer>
-        <p>&copy; 2023 Food Delivery</p>
-    </footer>
-
-    <script src="scripts.js"></script>
-</body>
-</html>
